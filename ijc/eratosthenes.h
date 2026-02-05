f01b6da0a50ff392908b40753ca86661507de3bf
oc-22 <cagalinec.o@gmail.com>
2026-02-05 18:25:51 +0100

    header file for eratosthenes.c

---
+++ ijc/eratosthenes.h
@@ -0,0 +1,13 @@
+// eratosthenes.c
+   // Řešení IJC-DU1, příklad a), 24.3.2025
+   // Autor: Ondrej Cagalinec, FIT
+   // Přeloženo: gcc Red Hat 14.2.1-7
+   // Hlaičkový soubor pro eratosthenes.c
+#ifndef eratosthenes_h
+#define eratosthenes_h
+
+#include "bitset.h"
+
+void Eratosthenes(bitset_t bitset_arr);
+
+#endif
